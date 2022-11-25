3baed670f19de29cb199c867000667496f2e38d8
shajn786 <shajn786@gmail.com>
2022-11-25 17:25:19 +0530

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CourseAddComponent } from './course-add/course-add.component';
 import { CourseViewAllComponent } from './course-view-all/course-view-all.component';
+
+const myRoutes: Routes=[
+  {
+    path:"",component:CourseAddComponent
+  },
+  {
+    path:"viewall",component:CourseViewAllComponent
+  }
+]
 
 @NgModule({
   declarations: [
@@ -14,7 +24,8 @@ import { CourseViewAllComponent } from './course-view-all/course-view-all.compon
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]

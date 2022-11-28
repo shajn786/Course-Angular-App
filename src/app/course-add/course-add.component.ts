bdcb8bb6aaf59f2c53facf2628210d3bcc586080
shajn786 <shajn786@gmail.com>
2022-11-28 12:06:00 +0530

    reading values

--- src/app/course-add/course-add.component.ts
+++ src/app/course-add/course-add.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class CourseAddComponent {
 
+  courseTitle=""
+  courseDescription=""
+  courseDuration=""
+  courseDate=""
+  courseVenue=""
+  
+  
+readValues = ()=>
+{
+  let data : any= {"courseTitle":this.courseTitle,"courseDescription":this.courseDescription,"courseDuration":this.courseDuration,"courseDate":this.courseDate,"courseVenue":this.courseVenue}
+  console.log(data)
+
+}
+
 }
